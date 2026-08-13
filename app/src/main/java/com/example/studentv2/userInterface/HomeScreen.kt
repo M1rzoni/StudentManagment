574390b0fb1d47bdb378542b9cf1dafc5402ed88
Mirza <113027159+M1rzoni@users.noreply.github.com>
2026-08-13 16:13:09 +0200

    Add Student creation dialog

--- app/src/main/java/com/example/studentv2/userInterface/HomeScreen.kt
+++ app/src/main/java/com/example/studentv2/userInterface/HomeScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +16,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -22,6 +28,32 @@ fun HomeScreen(
     modifier: Modifier = Modifier
 ){
 
+    var showDialog by remember() {
+        mutableStateOf(false)
+    }
+
+    if(showDialog){
+        AlertDialog(
+            onDismissRequest = {
+                showDialog = false
+            },
+            title = {
+                Text("Dodaj studenta")
+            },
+            text = {
+                Text("Dialog Radi")
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        showDialog = false
+                    }
+                ) {
+                    Text("OK")
+                }
+            }
+        )
+    }
 
     Scaffold(
         topBar = {
@@ -34,6 +66,7 @@ fun HomeScreen(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
+                    showDialog = true
 
                 }
             ) {
